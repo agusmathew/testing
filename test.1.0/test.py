dd6b6abf7388036e7e7a0bb2c106178281907d29
Prasoon <prasoon.s@eraminfotech.in>
2018-09-04 17:30:31 +0530

    added loop for checking multiple files

--- test.1.0/test.py
+++ test.1.0/test.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 import csv_to_dict
 import re
 class color:
@@ -11,6 +12,7 @@ class color:
    BOLD = '\033[1m'
    UNDERLINE = '\033[4m'
    END = '\033[0m'
+
 
 month_dict = {
 			  "January":"01",
@@ -31,69 +33,72 @@ month_dict = {
 
 booking = csv_to_dict.d
 # print booking
+n=int(raw_input("enter the number of files:"))
+for file_no in range(n):
+	file = open("site_data"+str(file_no)+".txt","r")
+	check_in = file.read()
 
-file = open("site_data.txt","r")
-check_in = file.read()
+	# extracting from text file
+	re_ticket_no = re.findall("Document\s+(\d+)",check_in)
+	re_owner = re.findall('Owner\s+([\w+\s\w+]+)',check_in)
+	re_date_of_issue = re.findall('DATE OF ISSUE\s+(.*)',check_in)
+	re_control_no = re.findall('BOOKING REF.\s+\w+\/(\w+)',check_in)
+	re_passenger_name = re.findall('PASSENGER NAME  NOT TRANSFERABLE\s+(\w+\/[\w+\s]+)\s+DATE OF ISSUE',check_in)
 
-# extracting from text file
-re_ticket_no = re.findall("Document\s+(\d+)",check_in)
-re_owner = re.findall('Owner\s+([\w+\s\w+]+)',check_in)
-re_date_of_issue = re.findall('DATE OF ISSUE\s+(.*)',check_in)
-re_control_no = re.findall('BOOKING REF.\s+\w+\/(\w+)',check_in)
-# cleaning
-ticket_no = re_ticket_no[0].strip() if re_ticket_no else None
-raw_owner = re_owner[0].strip() if re_owner else None
-raw_date_of_issue = re.findall('\d+\w+\d+',re_date_of_issue[0]) if re_date_of_issue else None
-re_month = re.findall('\D+',raw_date_of_issue[0]) if raw_date_of_issue else None
-control_no = re_control_no[0].strip() if re_control_no else None
-
-
-
-
-
-
-
-# printing the heading
-print color.BOLD+color.DARKCYAN+"\nTESTING RESULTS"+color.END
-
-
-# actual testing (testing from site values to db values)
-# checking ticket number
-if ticket_no:
-	if ticket_no in booking.values():
-		print "\n"+color.YELLOW+" TICKET NUMBER IN SITE IS CORRECT "+color.END+color.BLUE+" PASS"+color.END
-# checking AIRLINE name
-if raw_owner:
-	owner = ''
-	for each in raw_owner.split():
-		owner = owner + list(each)[0]
-	if owner in booking.values():
-		print "\n"+color.YELLOW+" AIRLINE NAME IN SITE IS CORRECT "+color.END+color.BLUE+" PASS"+color.END
-# checking date of issue
-if raw_date_of_issue:
-	date_of_issue = raw_date_of_issue[0]
-	month = re_month[0].strip() if re_month else None
-	raw_doi = date_of_issue.replace(month,"-"+month_dict[month]+"-")
-	year = raw_doi.split("-")[-1]
-	exc_doi = raw_doi.replace(year,"20"+year)
-	list_doi = exc_doi.split("-")
-	doi = list_doi[2]+"-"+list_doi[1]+"-"+list_doi[0]
-	# print doi
-if doi:
-	if doi in booking.values():
-		print "\n"+color.YELLOW+" DATE OF ISSUE IN SITE IS CORRECT "+color.END+color.BLUE+" PASS"+color.END
-if control_no:
-	if control_no in booking.values():
-		print "\n"+color.YELLOW+" CONTROL NUMBER IN SITE IS CORRECT "+color.END+color.BLUE+" PASS"+color.END
+	# cleaning
+	ticket_no = re_ticket_no[0].strip() if re_ticket_no else None
+	raw_owner = re_owner[0].strip() if re_owner else None
+	raw_date_of_issue = re.findall('\d+\w+\d+',re_date_of_issue[0]) if re_date_of_issue else None
+	re_month = re.findall('\D+',raw_date_of_issue[0]) if raw_date_of_issue else None
+	control_no = re_control_no[0].strip() if re_control_no else None
+	passenger_name = re_passenger_name[0].strip() if re_passenger_name else None
 
 
 
-# # general testing (testing db values in txt file of site)
-# for i in booking:
-# 	if booking[i].strip():
-# 		if booking[i].strip() in check_in:
-# 			print "\n"+str(i)+" ~~~ "+str(booking[i].strip())+color.BLUE+" PASS"+color.END
-# 		else:
-# 			print "\n"+str(i)+" ~~~ "+str(booking[i].strip())+color.RED+" FAILED"+color.END
-# 	else:
-# 		print "\n"+str(i)+" ~~~ "+color.GREEN+" NOT IN DB"+color.END
+
+	# printing the heading
+	print color.BOLD+color.DARKCYAN+"\nTESTING RESULTS"+color.END
+
+
+	# actual testing (testing from site values to db values)
+	# checking ticket number
+	if ticket_no:
+		if ticket_no in booking.values():
+			print "\n"+color.YELLOW+" TICKET NUMBER IS MATCHING "+color.END+color.BLUE+" PASS"+color.END,color.GREEN+ticket_no+color.END
+	# checking AIRLINE name
+	if raw_owner:
+		owner = ''
+		for each in raw_owner.split():
+			owner = owner + list(each)[0]
+		if owner in booking.values():
+			print "\n"+color.YELLOW+" AIRLINE NAME IS MATCHING "+color.END+color.BLUE+" PASS"+color.END
+	# checking date of issue
+	if raw_date_of_issue:
+		date_of_issue = raw_date_of_issue[0]
+		month = re_month[0].strip() if re_month else None
+		raw_doi = date_of_issue.replace(month,"-"+month_dict[month]+"-")
+		year = raw_doi.split("-")[-1]
+		exc_doi = raw_doi.replace(year,"20"+year)
+		list_doi = exc_doi.split("-")
+		doi = list_doi[2]+"-"+list_doi[1]+"-"+list_doi[0]
+		# print doi
+	if doi:
+		if doi in booking.values():
+			print "\n"+color.YELLOW+" DATE OF ISSUE IS MATCHING "+color.END+color.BLUE+" PASS"+color.END
+	if control_no:
+		if control_no in booking.values():
+			print "\n"+color.YELLOW+" CONTROL NUMBER IS MATCHING "+color.END+color.BLUE+" PASS"+color.END
+	if passenger_name:
+		if passenger_name in booking.get("pax_name").strip():
+			print "\n"+color.YELLOW+" PASSENGER NAME IS MATCHING "+color.END+color.BLUE+" PASS"+color.END
+
+
+	# # general testing (testing db values in txt file of site)
+	# for i in booking:
+	# 	if booking[i].strip():
+	# 		if booking[i].strip() in check_in:
+	# 			print "\n"+str(i)+" ~~~ "+str(booking[i].strip())+color.BLUE+" PASS"+color.END
+	# 		else:
+	# 			print "\n"+str(i)+" ~~~ "+str(booking[i].strip())+color.RED+" FAILED"+color.END
+	# 	else:
+	# 		print "\n"+str(i)+" ~~~ "+color.GREEN+" NOT IN DB"+color.END
